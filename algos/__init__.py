2861533bb394c044d96e9562bfdfa13d05c60805
Pawel Gajewski <lubiluk@gmail.com>
2020-11-13 10:18:31 +0100

    running trained policy

--- algos/__init__.py
+++ algos/__init__.py
@@ -1,1 +1,2 @@
 from .ddpg_her import ddpg_her
+from .logx import Logger, EpochLogger
---
+++ run_ddpg_her_0.py
@@ -0,0 +1,10 @@
+runninfrom algos.test_policy import load_policy_and_env, run_policy
+import gym
+import gym_pepper
+
+_, get_action = load_policy_and_env('./data/pepper_push_ddpg_her_0')
+
+env = gym.make('PepperPush-v0', gui=True)
+env.goal = "yea"
+
+run_policy(env, get_action)
