2692711e209bc370c32fb2ae5c4ad8fe2812d585
Paweł Gajewski <lubiluk@gmail.com>
2020-11-23 10:21:27 +0100

    run td3 her 1

---
+++ run/run_td3_her_1.py
@@ -0,0 +1,11 @@
+import gym
+import gym_pepper
+from gym.wrappers.time_limit import TimeLimit
+
+from algos.test_policy import load_policy_and_env, run_policy
+
+_, get_action = load_policy_and_env('./trained/pepper_push_td3_her_1')
+
+env = TimeLimit(gym.make('PepperPush-v0', gui=True, sim_steps_per_action=10), max_episode_steps=300)
+
+run_policy(env, get_action)
