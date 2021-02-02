422c7256b72b107143134ef5e2e827d2c0cf7d55
Pawel Gajewski <lubiluk@gmail.com>
2021-02-02 11:14:07 +0100

    cleanup

--- algos/__init__.py
+++ algos/__init__.py
@@ -1,7 +1,2 @@
-from .ddpg_her import ddpg_her
-from .td3_her import td3_her
-from .sac_her import sac_her
 from .td3_her_cam.td3_her_cam import td3_her_cam
-from .sac_her_s import sac_her_s
-from .sac import sac
 from .logx import Logger, EpochLogger
