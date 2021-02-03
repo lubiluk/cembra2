98c4edc60ad289df66b2dbe3317661c7c7762eff
Pawel Gajewski <lubiluk@gmail.com>
2021-02-03 12:30:41 +0100

    run fix

--- run_1.py
+++ run_1.py
@@ -6,7 +6,7 @@ from algos.test_policy import load_policy_and_env, run_policy
 
 _, get_action = load_policy_and_env('./data/1')
 
-env = TimeLimit(gym.make("PepperReach-v0", gui=True, dense=True),
+env = TimeLimit(gym.make("PepperPush-v0", gui=True, dense=True),
                      max_episode_steps=100)
 
 run_policy(env, get_action)
