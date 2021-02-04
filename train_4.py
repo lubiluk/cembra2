c8abb8c7cff237edbcfa64562cdeb9be98c07623
Pawel Gajewski <lubiluk@gmail.com>
2021-02-04 17:22:12 +0100

    fetch corrections

--- train_4.py
+++ train_4.py
@@ -6,13 +6,14 @@ import torch.nn as nn
 from algos import td3_her
 from algos.td3_her import core
 from gym.wrappers.time_limit import TimeLimit
+from utils.wrappers import DoneOnSuccessWrapper
 
 
 def env_fn():
-    return gym.make("FetchPush-v1", gui=False)
+    return DoneOnSuccessWrapper(gym.make("FetchPush-v1"))
 
 
-ac_kwargs = dict(hidden_sizes=[128, 128], activation=nn.ReLU)
+ac_kwargs = dict(hidden_sizes=[512, 512, 512], activation=nn.ReLU)
 
 logger_kwargs = dict(output_dir='data/1', exp_name='1')
 
@@ -22,13 +23,13 @@ td3_her(env_fn=env_fn,
         steps_per_epoch=10000,
         max_ep_len=100,
         epochs=100,
-        batch_size=64,
+        batch_size=2048,
         replay_size=100000,
         gamma=0.95,
         pi_lr=1e-3,
         q_lr=1e-3,
         update_after=1000,
-        update_every=64,
+        update_every=2048,
         num_additional_goals=4,
         goal_selection_strategy='future',
         logger_kwargs=logger_kwargs)
