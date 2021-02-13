14952d4300d82f4adf8cf7ee22014d0b2366cb9b
Pawel Gajewski <lubiluk@gmail.com>
2021-02-14 00:14:01 +0100

    train_0

--- sanity_2.py
+++ sanity_2.py
@@ -3,7 +3,7 @@ import gym
 import gym_pepper
 import torch
 import torch.nn as nn
-from algos import sac
+from algos import SAC
 from algos.sac import core_her
 from algos.common import her_replay_buffer
 
@@ -16,20 +16,20 @@ rb_kwargs = dict(size=1000000,
 
 logger_kwargs = dict(output_dir='data/sanity_2', exp_name='sanity_2')
 
-sac(env=env,
+model = SAC(env=env,
     actor_critic=core_her.MLPActorCritic,
     ac_kwargs=ac_kwargs,
     replay_buffer=her_replay_buffer.HerReplayBuffer,
     rb_kwargs=rb_kwargs,
-    steps_per_epoch=10000,
     max_ep_len=10000,
-    epochs=20,
     batch_size=256,
     gamma=0.95,
     lr=0.001,
     update_after=1000,
     update_every=1,
     logger_kwargs=logger_kwargs)
+
+model.train(steps_per_epoch=10000, epochs=20)
 
 from algos.test_policy import load_policy_and_env, run_policy
 
--- train_0.py
+++ train_0.py
@@ -2,31 +2,35 @@ import gym
 import gym_pepper
 import torch
 import torch.nn as nn
-from algos import td3
-from algos.td3 import core
+from algos import SAC
+from algos.sac import core
+from algos.common import replay_buffer
 from gym.wrappers.time_limit import TimeLimit
 
-
-def env_fn():
-    return TimeLimit(gym.make("PepperReach-v0", gui=False, dense=True),
-                     max_episode_steps=100)
-
+env = TimeLimit(gym.make("PepperReach-v0", gui=False, dense=True),
+                max_episode_steps=100)
 
 ac_kwargs = dict(hidden_sizes=[64, 64, 64], activation=nn.ReLU)
+rb_kwargs = dict(size=1000000)
 
 logger_kwargs = dict(output_dir='data/0', exp_name='0')
 
-td3(env_fn=env_fn,
-    actor_critic=core.MLPActorCritic,
-    ac_kwargs=ac_kwargs,
-    steps_per_epoch=10000,
-    max_ep_len=100,
-    epochs=100,
-    batch_size=64,
-    replay_size=100000,
-    gamma=0.95,
-    pi_lr=1e-3,
-    q_lr=1e-3,
-    update_after=1000,
-    update_every=64,
-    logger_kwargs=logger_kwargs)
+model = SAC(env=env,
+            actor_critic=core.MLPActorCritic,
+            ac_kwargs=ac_kwargs,
+            replay_buffer=replay_buffer.ReplayBuffer,
+            max_ep_len=100,
+            batch_size=64,
+            gamma=0.95,
+            lr=0.001,
+            update_after=1000,
+            update_every=64,
+            logger_kwargs=logger_kwargs)
+
+model.train(steps_per_epoch=10000, epochs=100)
+
+from algos.test_policy import load_policy_and_env, run_policy
+
+_, get_action = load_policy_and_env('data/0')
+
+run_policy(env, get_action)
