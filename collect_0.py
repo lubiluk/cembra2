9e809b411ef59e10b48178039b02d98518fcab46
Paul Gajewski <lubiluk@gmail.com>
2021-03-02 11:22:14 +0100

    collecting data

---
+++ collect_0.py
@@ -0,0 +1,39 @@
+import gym
+import gym_pepper
+import numpy as np
+import cv2
+import h5py
+from algos.common.utils import combined_shape
+
+N = 100000
+
+env = gym.make("PepperReachCam-v0", gui=True)
+obs = env.reset()
+
+img_dim = env.observation_space.spaces["camera"].shape
+lin_dim = env.observation_space.spaces["joints_state"].shape
+
+with h5py.File("/scratch/collect_0.hdf5", "w") as f:
+    img_dset = f.create_dataset("camera",
+                                combined_shape(N, img_dim),
+                                dtype='f')
+    lin_dset = f.create_dataset("joints_state",
+                                combined_shape(N, lin_dim),
+                                dtype='f')
+    pos_dset = f.create_dataset("object_position", (N, 3), dtype='f')
+
+    for j in range(N):
+        action = np.random.sample(10) * 2 - 1
+        o, r, d, i = env.step(action)
+        cv2.imshow("synthetic bottom camera", o["camera"])
+        cv2.waitKey(1)
+
+        img_dset[j] = o["camera"]
+        lin_dset[j] = o["joints_state"]
+        pos_dset[j] = i["object_position"]
+
+        if d:
+            env.reset()
+
+    env.close()
+
--- train_0.py
+++ train_0.py
@@ -7,8 +7,11 @@ from algos.sac import core
 from algos.common import replay_buffer
 from gym.wrappers.time_limit import TimeLimit
 
+torch.backends.cudnn.benchmark = True
+torch.autograd.set_detect_anomaly(False)
+torch.autograd.profiler.profile(enabled=False)
 
-env = TimeLimit(gym.make("PepperReach-v0", gui=False, dense=True),
+env = TimeLimit(gym.make("PepperReach-v0", gui=True, dense=True),
                 max_episode_steps=100)
 
 ac_kwargs = dict(hidden_sizes=[64, 64, 64], activation=nn.ReLU)
@@ -29,7 +32,7 @@ model = SAC(env=env,
             update_every=64,
             logger_kwargs=logger_kwargs)
 
-model.train(steps_per_epoch=10000, epochs=100)
+model.train(steps_per_epoch=10000, epochs=100, stop_success_rate=0.9)
 
 from algos.test_policy import load_policy_and_env, run_policy
 
