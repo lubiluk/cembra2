fd43aac69e7756821e93e8bfe3d334db7374b71b
Pawel Gajewski <lubiluk@gmail.com>
2021-03-15 13:48:55 +0100

    train 1

--- train_1.py
+++ train_1.py
@@ -2,8 +2,9 @@ import gym
 import gym_pepper
 import torch
 import torch.nn as nn
-from algos import td3_her
-from algos.td3_her import core
+from algos import SAC
+from algos.sac import core_her
+from algos.common import replay_buffer_her
 from gym.wrappers.time_limit import TimeLimit
 
 
@@ -11,28 +12,33 @@ torch.backends.cudnn.benchmark = True
 torch.autograd.set_detect_anomaly(False)
 torch.autograd.profiler.profile(enabled=False)
 
-def env_fn():
-    return TimeLimit(gym.make("PepperPush-v0", gui=False),
+env = TimeLimit(gym.make("PepperPush-v0", gui=False),
                      max_episode_steps=100)
 
 
 ac_kwargs = dict(hidden_sizes=[64, 64, 64], activation=nn.ReLU)
+rb_kwargs = dict(size=1000000)
 
 logger_kwargs = dict(output_dir='data/1', exp_name='1')
 
-td3_her(env_fn=env_fn,
-        actor_critic=core.MLPActorCritic,
-        ac_kwargs=ac_kwargs,
-        steps_per_epoch=10000,
-        max_ep_len=100,
-        epochs=100,
-        batch_size=64,
-        replay_size=100000,
-        gamma=0.95,
-        pi_lr=1e-3,
-        q_lr=1e-3,
-        update_after=1000,
-        update_every=64,
-        num_additional_goals=4,
-        goal_selection_strategy='future',
-        logger_kwargs=logger_kwargs)
+model = SAC(env=env,
+            actor_critic=core_her.MLPActorCritic,
+            ac_kwargs=ac_kwargs,
+            replay_buffer=replay_buffer_her.ReplayBuffer,
+            rb_kwargs=rb_kwargs,
+            max_ep_len=100,
+            batch_size=256,
+            gamma=0.95,
+            lr=0.001,
+            alpha=0.0002,
+            update_after=10,
+            update_every=1,
+            logger_kwargs=logger_kwargs)
+
+model.train(steps_per_epoch=1000, epochs=1000)
+
+from algos.test_policy import load_policy_and_env, run_policy
+
+_, get_action = load_policy_and_env('data/0')
+
+run_policy(env, get_action)
